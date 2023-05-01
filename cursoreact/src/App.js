78c3aa21a6b299d041b25fbc0a2232a7bd745af5
Isac de souza lima <113202848+isacdelima@users.noreply.github.com>
2023-05-01 17:39:39 -0300

    att

---
+++ cursoreact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div>
+      <h1>Site teste</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
