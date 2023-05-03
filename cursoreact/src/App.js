15f9cbbc228bdc46031d0c1a888060e4091bfd4b
Isac de souza lima <113202848+isacdelima@users.noreply.github.com>
2023-05-03 19:19:40 -0300

    Create Second Component

--- cursoreact/src/App.js
+++ cursoreact/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import FirstComponent from './components/FirstComponent';
+import SecondComponent from './components/SecondComponent'
 
 function App() {
   return (
     <div>
       <h1>Site teste</h1>
       <FirstComponent/>
+      <SecondComponent/>
     </div>
   );
 }
---
+++ cursoreact/src/components/SecondComponent.js
@@ -0,0 +1,12 @@
+function SecondComponent (){
+    return (
+        <ul> Livros:
+        <li>Dom Casmurro</li>
+        <li>O Alienista</li>
+        <li>Memórias póstumas de Brás Cubas</li>
+        <li>Dave Grohl: Uma história de vida</li>
+        </ul>
+    )
+}
+
+export default SecondComponent
