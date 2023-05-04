fc5097ec91493bd7bfef02bed249a68b1df35bb9
Isac de souza lima <113202848+isacdelima@users.noreply.github.com>
2023-05-03 21:03:08 -0300

    Ajustes de imagem

--- cursoreact/src/App.js
+++ cursoreact/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 
 import FirstComponent from './components/FirstComponent';
 import SecondComponent from './components/SecondComponent';
-
+import Images from './components/Images';
 function App() {
   return (
     <div>
       <h1>Site teste</h1>
       <FirstComponent/>
       <SecondComponent/>
+      <Images/>
     </div>
   );
 }
---
+++ cursoreact/src/components/Images.js
@@ -0,0 +1,12 @@
+import Cobra from "../assets/Cobra.jpg";
+
+const Images = () => {
+    return(
+        <div>
+            <img src="/tigre.png" alt="Imagem Tigre"/>
+            <img src={Cobra}/>
+        </div>
+    )
+}
+
+export default Images
